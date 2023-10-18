8991a27a03e39fde1796c65495346ebc0ea8b4a7
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-10-18 12:38:25 +0200

    FIX: autoplay

--- content.js
+++ content.js
@@ -1,9 +1,28 @@
-// content.js
-function hideEnforcementMessage() {
-    const enforcementMessage = document.querySelector('ytd-enforcement-message-view-model');
-    if (enforcementMessage) {
-        enforcementMessage.style.display = 'none';
+// Fonction pour vérifier et supprimer l'élément
+function supprimerElementAutomatiquement() {
+    var elementASupprimer = document.querySelector('ytd-enforcement-message-view-model');
+    var elementASupprimer2 = document.querySelector('tp-yt-iron-overlay-backdrop');
+    var videoPlayer = document.querySelector('video');
+    if (elementASupprimer) {
+        elementASupprimer.remove();
+        console.log('AH AHHHHHHH !');
+        if (videoPlayer) {
+            videoPlayer.play();
+        } else {
+            console.log("Aucun lecteur vidéo trouvé sur la page.");
+        }
     }
+    if (elementASupprimer2) {
+        elementASupprimer2.remove();
+        console.log('AH AHHHHHHH !');
+        if (videoPlayer) {
+            videoPlayer.play();
+        } else {
+            console.log("Aucun lecteur vidéo trouvé sur la page.");
+        }
+    }
+    console.log('RIEN A BLOQUE');
 }
 
-hideEnforcementMessage();
+// Vérifier et supprimer l'élément toutes les 1 seconde (ajustable)
+var intervalID = setInterval(supprimerElementAutomatiquement, 1000);
